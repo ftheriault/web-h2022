efbe61160f6585e82c1d9215a32ebce0774f0956
Frédéric Thériault <fred.theriault@gmail.com>
2022-01-25 11:20:14 -0500

    Done with loops

--- PHP - Base/Notes/02 - boucles.php
+++ PHP - Base/Notes/02 - boucles.php
@@ -6,6 +6,37 @@
 	</head>
 	<body>
 		<h1>Champs radio</h1>
+		<?php
+			for ($i = 0; $i < 10; $i++) {
+				?>
+				<div>
+					<input type="radio" name="<?= $i ?>"> <?= $i ?> hey!
+				</div>
+				<?php
+			}
+
+			$j = 10;
+
+			while ($j > 0) {
+				$j--;
+				echo $j;
+			}
+
+			$tab = []; // array();
+			$tab[] = "Hello"; // Si les crochets sont collés sur la variable, c'est un push
+			$tab[] = "World";
+
+			// phpr = inverse de phpt
+			foreach ($tab as $element) {
+				?>
+				<div>
+					<?= $element ?>
+				</div>
+				<?php
+			}
+
+			var_dump($tab);
+		?>
 	</body>
 </html>
 
