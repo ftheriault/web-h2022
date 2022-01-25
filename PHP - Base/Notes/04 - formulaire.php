af4d253788faa054f14e70116c8f8785472ba05d
Frédéric Thériault <fred.theriault@gmail.com>
2022-01-25 12:16:41 -0500

    Done with forms

--- PHP - Base/Notes/04 - formulaire.php
+++ PHP - Base/Notes/04 - formulaire.php
@@ -1,3 +1,11 @@
+<?php
+	// $_GET["info"] ou $_POST["info"] (si méthode = post)
+	$showBox = false;
+
+	if (!empty($_GET["info"]) && strlen($_GET["info"]) > 5) {
+		$showBox = true;
+	}
+?>
 <!DOCTYPE html>
 <html lang="fr">
 	<head>
@@ -5,6 +13,16 @@
 		<meta charset="utf-8" />
 	</head>
 	<body>
+		<?php
+			// !empty = variable existe, elle n'est pas nulle, elle n'est pas "", ni 0
+			if ($showBox) {
+				?>
+				<div style="color:green;background-color:#999;border:1px solid black">
+					Bravo!
+				</div>
+				<?php
+			}
+		?>
 		<form action="04 - formulaire.php" method="get">
 			<div>
 				Info : <input type="text" name="info" />
