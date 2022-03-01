a9151df6c22e841c8c5a05837640443d28accd4b
Frédéric Thériault <fred.theriault@gmail.com>
2022-03-01 10:26:42 -0500

    Added database connection

--- PHP - MVC - DB/action/DAO/UserDAO.php
+++ PHP - MVC - DB/action/DAO/UserDAO.php
@@ -1,20 +1,45 @@
 <?php
+    require_once("action/DAO/Connection.php");
 
     class UserDAO {
 
         public static function authenticate($username, $password) {
+            $connection = Connection::getConnection();
+
+            // ' or 1=1 or '1'='1
+            $statement = $connection->prepare("SELECT * FROM users WHERE username = ?");
+            $statement->bindParam(1, $username); // 1= premier ?
+            // $statement->bindParam(2, $lastName);
+            $statement->setFetchMode(PDO::FETCH_ASSOC); // Seulement lors de select = $row["first_name"], sinon $row[0]
+            $statement->execute();
+
             $result = null;
 
-            if($username == "john" && $password == "qwerty") {
-                $result = [];
-                $result["username"] = "Jonathan";
-                $result["visibility"] = 1;
+            if ($row = $statement->fetch()) {
+                // Il y a quelque chose!
+
+                // $hash = password_hash($password, PASSWORD_DEFAULT);
+                if (password_verify($password, $row["password"])) {
+                    $result = [];
+                    $result["username"] = $row["first_name"];
+                    $result["visibility"] = $row["visibility"];
+                }
             }
 
+            // $rows = $statement->fetchAll();
+            // foreach($rows as $row) {
+
+            // }
+            
             return $result;
         }
 
         public static function updateProfile($username, $newName, $newPassword) {
+            $connection = Connection::getConnection();
+
+            $statement = $connection->prepare("SELECT * FROM users WHERE username = '$username'");
+            $statement->setFetchMode(PDO::FETCH_ASSOC); // Seulement lors de select = $row["first_name"], sinon $row[0]
+            $statement->execute();
             // UPDATE users SET ...
         }
     }
---
+++ PHP - MVC - DB/action/constants.php
@@ -0,0 +1,12 @@
+<?php
+    define("DB_HOST", "pgsql:host=webpgsql.notes-de-cours.com;dbname=db_fred");
+    define("DB_USER", "fred");
+    define("DB_PASS", "AAAaaa111");
+
+    define("GST", 0.05);
+
+    define("DEFAULT_LANG", "fr");
+
+    define("MIN_PWD_LEN", 8);
+
+    // .env
